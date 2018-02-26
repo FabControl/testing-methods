dbe6d6b837e542107958e88b6edaec1d7f243945
BaconBad <egils.arajs@massportal.com>
2018-02-26 15:36:36 +0200

    File overwrite prevention function introduced as exclusive_write(path: str, output, limit = True)

--- CLI_helpers.py
+++ CLI_helpers.py
@@ -47,3 +47,33 @@ def extruded_filament(path):
             total_extrusion = total_extrusion + literal_eval(value)
 
     return round(total_extrusion)
+
+
+def exclusive_write(path: str, output, limit = True):
+    """
+    Takes a path and output stream, attempts to write the stream to the path,
+    if it exists, alters the path to either have a 3x zero padded number before the extension,
+    or add padding and increase an existing number by 1.
+    A boolean limit argument is used to prevent the file writing from overflowing and writing
+    too many instances of a file.
+
+    Used to prevent file overwriting.
+
+    split[0] is the filename,
+    split[1] is either a number or NoneType
+    split[2] is a file extension
+    :param path:
+    :param output:
+    :param limit:
+    :return:
+    """
+    try:
+        with open(path, "x") as file:
+            file.write(output)
+    except:
+        split = re.split(r'(\d+(?=\.))*(\.[0-9a-zA-Z]+?$)', path)
+        if split[1] is not None and int(split[1]) > 999 and limit:
+            raise ValueError('%s%s is likely overflowing and has reached 1000 or more instances.' %(split[0], split[2]))
+        path = split[0] + str(int(split[1] if split[1] is not None else 0) + 1).zfill(3) + split[2]
+        with open(path, "x") as file:
+            file.write(output)
