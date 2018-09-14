4e0c57d8cc72d9fe14ba56722d49fd7a6d13bcff
BaconBad <egils.arajs@massportal.com>
2018-09-14 14:55:43 +0300

    fixed generate_suggested_values.py

--- generate_suggested_values.py
+++ generate_suggested_values.py
@@ -19,13 +19,12 @@ material = Material(**persistence["material"])
 machine = Machine(**persistence["machine"])
 machine.settings = Settings(**persistence["settings"])
 
-twr = border_values(minmax_track_width_height_raft(machine, 7)[3])
 
 suggested_values = {"temperature": border_values(minmax_temperature(material, machine, 7)),
                    "track_height": [x * machine.nozzle.size_id for x in border_values(minmax_track_height(machine, 7))],
                    "track_width": [x * machine.nozzle.size_id for x in border_values(list(minmax_track_width(machine, 7)[0]))],
                    "track_width_raft": border_values(minmax_track_width_height_raft(machine, 7)[2]),
-                   "track_height_raft": [x * machine.nozzle.size_id for x in border_values(minmax_track_width_height_raft(machine, 7)[3])],
+                   "track_height_raft": [x * machine.nozzle.size_id for x in border_values(minmax_track_width_height_raft(machine, 7)[2])],
                    "extrusion_multiplier": border_values([test_dict["06"].min_default, test_dict["06"].max_default]),
                    "retraction_distance": border_values([test_dict["08"].min_default, test_dict["08"].max_default]),
                    "test_name": test_info.name}
