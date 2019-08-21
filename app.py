6af964af9a9db8e9f917f362d98407ad91bc5b2a
egilsar <egils.arajs@massportal.com>
2019-08-21 15:58:37 +0300

    Created a new API endpoint /routine. To access overview of the testing procedure. Removed unnecessary imports.

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 #!flask/bin/python
 from flask import Flask, jsonify, make_response, abort, request, send_file
 from persistence import Persistence
-import os
-from fill_values import fill_values
 from initialize_test import OptimizerSession
 from base64 import b64encode
 
@@ -33,50 +31,28 @@ def test_json():
     return jsonify(persistence.dict)
 
 
-# @app.route('/tasks', methods=['GET'])
-# def get_tasks():
-#     return jsonify({'tasks': tasks})
+@app.route('/routine', methods=['GET', 'POST'])
+def get_routine():
+    routine = {
+        "01": {"name": "first-layer track height vs first-layer printing speed", "priority": "primary"},
+        "02": {"name": "first-layer track width", "priority": "secondary"},
+        "03": {"name": "extrusion temperature vs printing speed", "priority": "primary"},
+        "05": {"name": "track width", "priority": "secondary"},
+        "06": {"name": "extrusion multiplier vs printing speed", "priority": "secondary"},
+        "07": {"name": "printing speed", "priority": "primary"},
+        "08": {"name": "extrusion temperature vs retraction distance", "priority": "primary"},
+        "09": {"name": "retraction distance vs printing speed", "priority": "secondary"},
+        "10": {"name": "retraction distance", "priority": "secondary"},
+        "11": {"name": "retraction distance vs retraction speed", "priority": "secondary"},
+        "12": {"name": "retraction-restart distance vs coasting distance", "priority": "secondary"},
+        "13": {"name": "bridging extrusion multiplier vs bridging printing speed", "priority": "primary"}
+    }
+    return jsonify(routine)
 
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-# @app.route('/tasks/<int:task_id>', methods=['GET'])
-# def get_task(task_id):
-#     task = [task for task in tasks if task["id"] == task_id]
-#     if len(task) == 0:
-#         abort(404)
-#     return jsonify({'task': task[0]})
-#
-#
-# @app.route('/tasks', methods=['POST'])
-# def create_task():
-#     if not request.json or 'title' not in request.json:
-#         abort(400)
-#     task = {
-#         'id': tasks[-1]['id'] + 1,
-#         'title': request.json['title'],
-#         'description': request.json.get('description', ""),
-#         'done': False
-#     }
-#     tasks.append(task)
-#     return jsonify({'task': task}), 201
-
-
-# @app.route('/')
-# def index():
-#     strIO = StringIO.StringIO()
-#     strIO.write('Hello from Dan Jacob and Stephane Wirtel !')
-#     strIO.seek(0)
-#     return send_file(strIO,
-#                      attachment_filename="testing.txt",
-#                      as_attachment=True)
-
-# return send_file(buffer, as_attachment=True,
-#                      attachment_filename='a_file.txt',
-#                      mimetype='text/csv')
 
 
 if __name__ == '__main__':
