eef5ec55151a17573b8dc955680785d636e730d6
Aivars Vaivods <aivars.vaivods@edu.rtu.lv>
2019-10-30 14:02:26 +0200

    Added test helper class

--- application_unit_tests/test_routes.py
+++ application_unit_tests/test_routes.py
@@ -1,11 +1,14 @@
 from flask_testing import TestCase
 from app import app
 
-class NotFoundRoute(TestCase):
+
+# subclass this instead of TestCase
+class CreateAppHelperClass(TestCase):
     def create_app(self):
         app.config['TESTING'] = True
         return app
 
+class NotFoundRoute(CreateAppHelperClass):
     def test_route_response(self):
         resp = self.client.get('/some_not_existing_endpoint/')
         self.assert404(resp)
