c80c3b4e9075f993850763d420462695fb67dafd
Aivars Vaivods <aivars.vaivods@edu.rtu.lv>
2020-03-25 10:39:26 +0200

    Fixed missing E args from g-code
    Bug untroduced in 61c4ccd commit

--- GcodeStuff.py
+++ GcodeStuff.py
@@ -303,7 +303,7 @@ class Gplus(G):
 
     def write(self, statement_in, resp_needed=False):
         # Ensure that E0.00000 is never passed to gcode
-        statement_in = re.sub(r"E0\.0+", '', statement_in)
+        statement_in = re.sub(r"E0\.0+([^0-9]|$)", r'\1', statement_in)
         self._gcode.append(statement_in)
 
     def teardown(self, wait=True):
