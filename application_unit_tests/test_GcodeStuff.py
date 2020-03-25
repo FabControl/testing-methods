94ff53629e61515fe7e6808a155efd1d7aa43979
Aivars Vaivods <aivars.vaivods@edu.rtu.lv>
2020-03-25 20:17:42 +0200

    Added filament diameter test

--- application_unit_tests/test_GcodeStuff.py
+++ application_unit_tests/test_GcodeStuff.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from GcodeStuff import Gplus
 from Definitions import Material, Machine
+import re
 
 
 class TestGplus(TestCase):
@@ -12,27 +13,48 @@ class TestGplus(TestCase):
             ('G92 E0','G92 E0'),
             ('G1 X0.0000 Y0.0000 E0.001','G1 X0.0000 Y0.0000 E0.001')
             ]
+    M = Machine(model="",
+                temperature_controllers=dict(
+                        extruder={"temperature_max": 100,
+                                  "part_cooling": True,
+                                  "nozzle": {"size_id": 0.4}},
+                        chamber={"chamber_heatable": False},
+                        printbed={"printbed_heatable": False}))
+
+    M.settings = object()
+    M.settings = type('settings_replacement', (), 
+                        dict(extrusion_multiplier=1.0,
+                            speed_printing=50,
+                            retraction_speed=50,
+                            track_height=0.1,
+                            track_width=0.2))()
+
+
     def test_E0(self):
-        M = Machine(model="",
-                    temperature_controllers=dict(
-                            extruder={"temperature_max": 100,
-                                      "part_cooling": True,
-                                      "nozzle": {"size_id": 0.4}},
-                            chamber={"chamber_heatable": False},
-                            printbed={"printbed_heatable": False}))
 
-        M.settings = object()
-        M.settings = type('settings_replacement', (), 
-                            dict(extrusion_multiplier=1.0,
-                                speed_printing=50,
-                                retraction_speed=50,
-                                track_height=0.1,
-                                track_width=0.2))()
-
-        G = Gplus(Material("mat", 1.75),
-                M)
+        G = Gplus(Material("mat", 1.75), self.M)
 
         for (inCode, expected) in self._E0_TEST_GCODE:
             with self.subTest(expected=expected):
                 G.write(inCode)
                 self.assertEqual(G.gcode.split('\n')[-1], expected)
+
+
+    def test_filament_diameter(self):
+        G1 = Gplus(Material("mat", 1.0), self.M)
+        G2 = Gplus(Material("mat", 2.0), self.M)
+        G1.move(x=200, extrude=True)
+        G2.move(x=200, extrude=True)
+
+        e1 = re.search(r'E([0-9]+\.[0-9]+)', G1.gcode)
+        self.assertIsNotNone(e1)
+
+        e2 = re.search(r'E([0-9]+\.[0-9]+)', G2.gcode)
+        self.assertIsNotNone(e2)
+
+        length1 = round(float(e1.group(1)), 5)
+        length2 = round(float(e2.group(1)), 5)
+
+        # might fail if g-code command uses less than 6 decimal places
+        self.assertEqual(length1, length2 * 2.0 ** 2)
+
