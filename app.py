3f2efdec650055b135a91000773c59ae7ae66451
egilsar <egils.arajs@massportal.com>
2020-08-14 16:35:19 +0300

    Added a 'beta' tag to test 14

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def get_routine():
         "11": {"name": "Retraction distance vs Retraction speed", "priority": "secondary"},
         "12": {"name": "Retraction restart distance vs Printing speed and Coasting distance", "priority": "secondary"},
         "13": {"name": "Bridging extrusion multiplier vs Bridging printing speed", "priority": "primary"},
-        "14": {"name": "Soluble support adhesion", "priority": "secondary"}
+        "14": {"name": "Soluble support adhesion (beta)", "priority": "secondary"}
     }
     return jsonify(routine)
 
