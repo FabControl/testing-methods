bd00ed55896225357a7cccb6193c89bc6eff73b6
egilsar <egils.arajs@massportal.com>
2020-09-22 13:57:20 +0300

    Added detachable_support values generator

--- get_values_A.py
+++ get_values_A.py
@@ -96,7 +96,7 @@ class get_values_A(object):
         self.bridging_speed_printing = machine.settings.bridging_speed_printing
 
         # Support parameters
-        self.support_pattern_spacing = 2.5
+        self.support_pattern_spacing = 50
         self.support_contact_distance = 0.2
 
         self.offset_z = 0
@@ -297,13 +297,17 @@ class get_values_A(object):
             self.speed_printing = self.parameter_two.values
 
         elif self.test_number == "15":
-            # DETACHABLE SUPPORT test parameters
+            # SUPPORT SPACING vs SUPPORT CONTACT DISTANCE test parameters
             self.support_pattern_spacing = np.linspace(fixed_parameter_values.parameter_one.values[0],
-                                                           fixed_parameter_values.parameter_one.values[-1],
-                                                           self.number_of_test_structures).tolist()
+                                                       fixed_parameter_values.parameter_one.values[-1],
+                                                       self.number_of_test_structures).tolist()
+            self.parameter_one.values = self.support_pattern_spacing
+
             self.support_contact_distance = np.linspace(fixed_parameter_values.parameter_two.values[0],
-                                              fixed_parameter_values.parameter_two.values[-1],
-                                              self.number_of_substructures).tolist()
+                                                            fixed_parameter_values.parameter_two.values[-1],
+                                                            self.number_of_test_structures).tolist()
+            self.parameter_two.values = self.support_contact_distance
+
         else:
             raise ValueError("{} is not a valid test.".format(fixed_parameter_values.name))
 
